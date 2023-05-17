2794f8a990227e91c2f0bcdd24c5419d206d0a7b
gkiplangat <gideonkiplangat4@gmail.com>
2023-05-17 11:18:28 -0700

    betty for exit

--- exit.c
+++ exit.c
@@ -1,7 +1,13 @@
 #include "shell.h"
 #include <stdlib.h>
 
-void exit_shell() {
-    write(STDOUT_FILENO, "Exiting ...\n", 12);
-    exit(0);
+/**
+ * exit_shell - exit shell
+ * Return: 0 success
+ */
+
+void exit_shell(void)
+{
+	write(STDOUT_FILENO, "Exiting ...\n", 12);
+	exit(0);
 }
