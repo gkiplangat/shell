260b2849cb7a4f32d1ded759670d0fa8270224fa
gkiplangat <gideonkiplangat4@gmail.com>
2023-05-20 08:14:19 -0700

    solved betty errors and warnings for enviroment.c

--- enviroment.c
+++ enviroment.c
@@ -1,12 +1,19 @@
 #include "shell.h"
 
+/**
+ * print_environment - print enviroment variables
+ *
+ * Return: void
+ */
 
-void print_environment() {
-    char **env = environ;
+void print_environment(void)
+{
+	char **env = environ;
 
-    while (*env != NULL) {
-        printf("%s\n", *env);
-        env++;
-    }
+	while (*env != NULL)
+	{
+	printf("%s\n", *env);
+	env++;
+	}
 }
 
